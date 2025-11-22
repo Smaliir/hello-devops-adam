f2360331c2c8f22fe25569782bffd199cb3ac264
Smaliir <167239033+Smaliir@users.noreply.github.com>
2025-11-22 11:54:45 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "hello-devops",
+  "version": "1.0.0",
+  "description": "Minimal Hello World app for DevOps demo",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js",
+    "build": "echo 'Build step: nincs fordítás, demonstráció'",
+    "lint": "node -e \"console.log('lint pass')\""
+  },
+  "author": "",
+  "license": "MIT",
+  "dependencies": {}
+}
